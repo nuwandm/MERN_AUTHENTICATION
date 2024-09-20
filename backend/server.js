b78702bca0713553ed61849a1045b21771879459
Nuwan Madusanka <nuwandarshana2012@gmail.com>
2024-09-20 18:24:28 +0530

    creating server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+// middleware
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const server = async () => {
-	console.log("server is running.");
+	try {
+		console.log("server is running on port :", port);
+	} catch (error) {
+		console.log("Starting Failed", error.message);
+		process.exit(1);
+	}
 };
 server();
