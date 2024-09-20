2efd0b7b28c399070a157d9220a519e3b3658943
Nuwan Madusanka <nuwandarshana2012@gmail.com>
2024-09-21 00:01:59 +0530

    added routes

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,11 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connect from "./src/db/connect.js";
 import cookieParser from "cookie-parser";
+import fs from "node:fs";
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 const app = express();
 
@@ -14,12 +15,27 @@ const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser);
- 
+app.use(cookieParser());
+
+// routes
+const routFiles = fs.readdirSync("./src/routes");
+
+routFiles.forEach((file) => {
+	import(`./src/routes/${file}`)
+		.then((route) => {
+			app.use("/api/v1", route.default);
+		})
+		.catch((err) => {
+			console.log("failed to load route file", err);
+		});
+});
+
 const server = async () => {
 	try {
 		await connect();
-		console.log("server is running on port :", port);
+		app.listen(port, () => {
+			console.log("server is running on port :", port);
+		});
 	} catch (error) {
 		console.log("Starting Failed", error.message);
 		process.exit(1);
